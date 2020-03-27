e1c774d88b1e223998432a33554d13976051ec7f
delmarhirata <delmar.hirata@aluno.faculdadeimpacta.com.br>
2020-03-26 21:48:25 -0300

    JavaScript

---
+++ funcoes.js
@@ -0,0 +1,28 @@
+//alert("Aula de javaScript");
+//comentário 1 linha
+
+/*linha1
+linha2
+linha3
+Várias linhas
+
+
+var numero = 2020;
+var nome="Delmar";
+var carac='';
+var x = "Noite";
+var y = "Dia";
+alert(x + y);
+...*/
+var x = 5;
+var y = 2;
+
+if(x==y){
+    alert ("iguais");
+}
+else if (x>y){
+    alert("mmaio");
+}
+else{
+    alert("menor");
+}
